1bdb01c47803c88a9bf5e6750bc3cb76ad75aea2
amnahid <nahidshikder60@gmail.com>
2024-03-02 16:30:32 +0600

    mongodb bug fix

--- src/index.js
+++ src/index.js
@@ -8,12 +8,17 @@ require('dotenv').config()
 // Middleware
 app.use(bodyParser.json());
 // static
-app.use('/',express.static('public'))
+app.use('/', express.static('public'))
 // MongoDB Connection
-mongoose.connect(process.env.FLASH_KISS_DB)
-.then(() => console.log('MongoDB Connected'))
-.catch(err => console.log(err));
-
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+}
 // Routes
 const authRoutes = require('./routes/auth.route');
 const subscriptionRoutes = require('./routes/subscription.route');
@@ -22,4 +27,9 @@ app.use('/api/auth', authRoutes);
 app.use('/api/subscriptions', subscriptionRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+//Connect to the database before listening
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log("listening for requests");
+    })
+})
